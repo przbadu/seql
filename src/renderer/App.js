ef54772c0b51e867d2ba4cef8a48adef2953aeb6
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2021-02-05 20:49:23 +0545

    setup react js with electron

---
+++ src/renderer/App.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const App = () => <h2>hello from React!</h2>;
+
+ReactDOM.render(<App />, document.querySelector("#root"));
